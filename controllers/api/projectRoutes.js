bd1562ee49eb82dcf3faa4a04a22ef54c8e8bcf0
Tim Davidson <timdavidson2@gmail.com>
2021-09-11 13:58:07 -0400

    update homepage

--- controllers/api/projectRoutes.js
+++ controllers/api/projectRoutes.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const { Project } = require('../../models');
 const withAuth = require('../../utils/auth');
-const apikey =
-  'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=Zspd4UDESTDcvhmmO3bUNljh3NXsNuP4gTc63oXO';
-const apiurl = '[ Base URL: developer.nps.gov/api/v1 ]';
 
 router.post('/', withAuth, async (req, res) => {
   try {
