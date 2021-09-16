4f929f052c62a7455fb502098b021f6e0cd696de
Melaku Sheri <mailangels@gmail.com>
2021-09-15 20:36:31 -0400

    controller and seed added

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,61 +1,70 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create(req.body);
+   try {
+    const userData = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+   });
 
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
+   req.session.save(() => {
+    req.session.user_id = userData.id;
+    req.session.email = userData.email;
+    req.session.loggedIn = true;
 
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
+    res.json(userData);});
+   } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+   }
+  
 });
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
-
+    userData = await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    });
     if (!userData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+      res.status(400).json({ message: 'No user with that email address!' });
       return;
     }
 
-    const validPassword = await userData.checkPassword(req.body.password);
+    const validPassword = userData.checkPassword(req.body.password);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect email or password, please try again' });
+      res.status(400).json({ message: 'Incorrect password!' });
       return;
     }
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
+      req.session.email = userData.email;
+      req.session.loggedIn = true;
+
       res.json({ user: userData, message: 'You are now logged in!' });
     });
 
+    
   } catch (err) {
-    res.status(400).json(err);
-  }
-});
+      console.log(err);
+      res.status(500).json(err);
+  } 
+ });
 
-router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
+ router.post('/logout', withAuth, (req, res) => {
+   if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }else {
+      res.status(404).end();
+    }
 });
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,84 +1,54 @@
 const router = require('express').Router();
-const { Project, User } = require('../models');
+const { County, User, Museum, Park } = require('../models');
 const withAuth = require('../utils/auth');
-const APIKEY = 'Zspd4UDESTDcvhmmO3bUNljh3NXsNuP4gTc63oXO';
-const APISEARCHPARKS = 'https://developer.nps.gov/api/v1/parks?&api_key=Zspd4UDESTDcvhmmO3bUNljh3NXsNuP4gTc63oXO';
 
 router.get('/', async (req, res) => {
-  try {
-    // Get all projects and JOIN with user data
-    const projectData = await Project.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
+   try {
+      console.log(req.session);
+      countyData = await County.findAll();
 
-    // Serialize data so the template can read it
-    const projects = projectData.map((project) => project.get({ plain: true }));
-
-    // Pass serialized data and session flag into template
-    res.render('homepage', {
-      projects,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-router.get('/project/:id', async (req, res) => {
-  try {
-    const projectData = await Project.findByPk(req.params.id, {
-      include: [
-        {
-          model: User,
-          attributes: ['name'],
-        },
-      ],
-    });
-
-    const project = projectData.get({ plain: true });
-
-    res.render('project', {
-      ...project,
-      logged_in: req.session.logged_in,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Project }],
-    });
-
-    const user = userData.get({ plain: true });
-
-    res.render('profile', {
-      ...user,
-      logged_in: true,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
+      console.log(countyData[0]);
+         const Counties = countyData.map(county => county.get({ plain: true }));
+         res.render('homepage', { 
+            Counties, 
+            loggedIn: req.session.loggedIn});
+      
+   } catch (error) {
+      console.log(err);
+         res.status(500).json(err);
+   }
 });
 
 router.get('/login', (req, res) => {
-  // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
-  }
+   if (req.session.loggedIn) {
+     res.redirect('/');
+     return;
+   }
+ 
+   res.render('loginandout');
+});
 
-  res.render('login');
+router.get('/county/:id', withAuth, async (req, res) => {
+   try {
+      const countyData = await County.findByPk(req.params.id);
+      const parkData = await Park.findAll({where: {county_id: req.params.id}});
+      const museumData = await Museum.findAll({where: {county_id: req.params.id}});
+
+      const county = countyData.get({ plain: true });
+      const park = parkData.map((parks) => parks.get({ plain: true }));
+      const museum = museumData.map((museum) => mu.get({ plain: true }));
+
+     // res.status(200).json({ county, park, museum });
+      res.render('county', {
+         county,
+         park,
+         museum,
+         loggedIn: req.session.loggedIn
+      });
+   } catch (err) {
+      console.log(err)
+      res.status(500).json(err);
+   }
 });
 
 module.exports = router;
